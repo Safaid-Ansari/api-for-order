838b512a97f57838f144bc66fef726bf382188c0
Safaid Ansari <safaidansari07@gmail.com>
2022-11-10 01:13:55 +0530

    Challenges Completed for Voosh

---
+++ config/database.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  // "mongodb+srv://SafaidAnsari:iBcR2P7fTHF4UZBx@cluster0.vmqnisk.mongodb.net/Order-API",
+  "mongodb://localhost/Order-API",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
+
+db.once("open", function () {
+  console.log("Connected to Database :: MongoDB");
+});
+
+module.exports = db;
